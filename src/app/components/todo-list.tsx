36e25f9c1136d4fc78f284907137b206f5a6d5a0
Fabii Kelvans <fkelvans@gmail.com>
2023-09-05 09:40:59 +0300

    build

--- src/app/components/todo-list.tsx
+++ src/app/components/todo-list.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 
 import { trpc } from "../_trpc/client";
 import { serverClient } from "../_trpc/server-client";
+import {Input} from "@/app/components/ui/input";
+import {Button} from "@/app/components/ui/button";
 
 export default function TodoList({
   initialTodos,
@@ -51,23 +53,23 @@ export default function TodoList({
       </div>
       <div className="flex gap-3 items-center">
         <label htmlFor="content">Content</label>
-        <input
+        <Input
           id="content"
           value={content}
           onChange={(e) => setContent(e.target.value)}
           className="flex-grow text-black bg-white rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 px-4 py-2"
         />
-        <button
+        <Button
           onClick={async () => {
             if (content.length) {
               addTodo.mutate(content);
               setContent("");
             }
           }}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          variant='default'
         >
-          Add Todo
-        </button>
+          Add
+        </Button>
       </div>
     </div>
   );
